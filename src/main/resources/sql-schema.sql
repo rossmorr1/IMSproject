b73e7876f8c4c4c6cbfbf70ab8dbcfddd555f8c7
rossmorr1 <rossmorr97@gmail.com>
2022-07-21 21:13:30 +0100

    created database structure

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+create table if not exists Product(
+ProductID int primary key auto_increment,
+ProductName varchar(50) not null,
+Price decimal(10,2) not null
+);
+
+create table if not exists OrderRecord(
+OrderID int primary key auto_increment,
+CustomerID int not null,
+foreign key (CustomerID) references Customer(CustomerID)
+);
+
+
+create table if not exists OrderProduct(
+id int primary key auto_increment,
+OrderId int not null,
+ProductID int not null,
+foreign key (OrderID) references Ordered(OrderID),
+foreign key (ProductID) references product(ProductID)
+);
